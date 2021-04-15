ccb0a2e81da0f9634e1141d66d47cbb2dea8233e
Lucas Ribeiro <lucasrb04@gmail.com>
2021-04-15 19:28:18 -0300

    adicionando requisito 3

--- script.js
+++ script.js
@@ -17,15 +17,19 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui.
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  event.target.remove();
+}
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -36,19 +40,12 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function getSkuFromProductItem(item) {
-  console.log(item);
-  return item.querySelector('span.item__sku').innerText;
-}
-
-async function shoppedItem(items) {
-  const sku = getSkuFromProductItem(items.target.parentNode);
-  console.log(sku);
-  // items.target.parentNode.firstChild.innerText;
+async function shoppedItem(event) {
+  const sku = getSkuFromProductItem(event.target.parentNode);
+  // const sku = items.target.parentNode.firstChild.innerText; Solução feita junto com Emerson
   const item = await getItemById(sku);
   const parentItem = document.querySelector('ol');
   const result = createCartItemElement(item);
-  console.log(result);
   parentItem.appendChild(result);
 }
 
